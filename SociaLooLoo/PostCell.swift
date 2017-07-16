388eff011e38faa8f56565d886e203cb6ee97c90
Dzhon <johnpeppe@gmail.com>
2017-07-16 20:15:38 +0300

    Comlpeted Firebase Display

--- SociaLooLoo/PostCell.swift
+++ SociaLooLoo/PostCell.swift
@@ -9,16 +9,24 @@
 import UIKit
 
 class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    var post: Post!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    @IBOutlet weak var profileImg: UIImageView!
-    @IBOutlet weak var userNameLbl: UILabel!
-    @IBOutlet weak var postImg: UIImageView!
-    @IBOutlet weak var caption: UITextView!
-    @IBOutlet weak var likesLvl: UILabel!
+    
+    func configureCell(post:Post) {
+        self.post = post
+        self.caption.text = post.caption
+        self.likesLbl.text = "\(post.likes)"
+    }
 
 }
